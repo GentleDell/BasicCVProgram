15f6b10b6ad83289dfe00de8624c60eb3185c8f1
Gentle Dell <hit_zhantaodeng@126.com>
2018-07-17 21:37:50 +0800

    modified GSLtest to make the rootsfinding a subfunction.

--- GSLtest.cpp
+++ GSLtest.cpp
@@ -2,25 +2,32 @@
 #include <stdio.h>
 #include <gsl/gsl_poly.h>
 
-int main (void)
+
+bool find_polynomialroots (double coeff[], double roots[], int size)
 {
-  int i;
+  int ct_i;
   /* coefficients of P(x) =  -1 + x^5  */
-  double a[6] = { -1, 0, 0, 0, 0, 1 };
-  double z[10];
+//  double a[6] = { -1, 0, 0, 0, 0, 1 };
+//  double z[10];
 
-  gsl_poly_complex_workspace * w = gsl_poly_complex_workspace_alloc (6);
+  gsl_poly_complex_workspace * w = gsl_poly_complex_workspace_alloc (size);
 
-  gsl_poly_complex_solve (a, 6, w, z);
+  gsl_poly_complex_solve (coeff, size, w, roots);
 
   gsl_poly_complex_workspace_free (w);
 
-  for (i = 0; i < 5; i++)
+  for (ct_i = 0; ct_i < size-1; ct_i++)
     {
       printf ("z%d = %+.18f %+.18f\n",
-              i, z[2*i], z[2*i+1]);
+              ct_i, roots[2*ct_i], roots[2*ct_i+1]);
     }
 
   return 0;
 }
 
+int main(void){
+    int size = 6;
+    double a[6] = { -1, 0, 0, 0, 0, 1 };
+    double roots[10];
+    return find_polynomialroots(a,roots,size);
+}
